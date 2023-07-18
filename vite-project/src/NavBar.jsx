61caf98237d314a07d01d278e7e2a50e846744db
notleo <notleo@student.ubc.ca>
2023-07-18 05:20:09 -0700

    Changed navbar text size to smaller

--- vite-project/src/NavBar.jsx
+++ vite-project/src/NavBar.jsx
@@ -3,7 +3,7 @@ const NavBar = () => {
         <section className="pt-[63px]">
             <nav className="flex justify-between">
                 <img className="cursor-pointer" src="/Logo.svg"></img>
-                <ul className="flex gap-[57px] text-2xl items-center text-brown">
+                <ul className="flex gap-[57px] text-xl items-center text-brown">
                     <li className="cursor-pointer">home</li>
                     <li className="cursor-pointer">about</li>
                     <li className="cursor-pointer">hikes</li>
